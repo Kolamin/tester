f2170f2a51f423e08c060583055ce48bebefd006
Anton Zhiliakov <gofranich@gmail.com>
2021-03-02 11:52:49 +0600

    Add correct answer, add notification

--- src/main/java/ru/anton/tester/view/MainView.java
+++ src/main/java/ru/anton/tester/view/MainView.java
@@ -1,24 +1,34 @@
 package ru.anton.tester.view;
 
 
+import com.vaadin.flow.component.html.Div;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.router.HighlightConditions;
 import com.vaadin.flow.router.Route;
+import com.vaadin.flow.router.RouterLink;
 import org.springframework.beans.factory.annotation.Autowired;
 
 @Route
 public class MainView extends VerticalLayout {
 
 
-    QuestionForm questionForm;
+    //QuestionForm questionForm;
 
 
     @Autowired
-    public MainView(QuestionForm questionForm) {
-        this.questionForm = questionForm;
+    public MainView() {
+        // this.questionForm = questionForm;
 
-        add(questionForm);
+
+
+        Div menu = new Div();
+
+        RouterLink testLink = new RouterLink("Г.2.1 Эксплуатация тепловых энергоустановок и тепловых сетей",
+                QuestionForm.class);
+        testLink.setHighlightCondition(HighlightConditions.sameLocation());
+        menu.add(testLink);
+
+        add(menu);
 
     }
-
-
 }
--- src/main/java/ru/anton/tester/view/QuestionForm.java
+++ src/main/java/ru/anton/tester/view/QuestionForm.java
@@ -1,23 +1,32 @@
 package ru.anton.tester.view;
 
 
+import com.vaadin.flow.component.UI;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.button.ButtonVariant;
 import com.vaadin.flow.component.html.H3;
+import com.vaadin.flow.component.html.Span;
+import com.vaadin.flow.component.notification.Notification;
+import com.vaadin.flow.component.notification.NotificationVariant;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.radiobutton.RadioButtonGroup;
-import com.vaadin.flow.spring.annotation.SpringComponent;
-import com.vaadin.flow.spring.annotation.UIScope;
+import com.vaadin.flow.component.radiobutton.RadioGroupVariant;
+import com.vaadin.flow.router.PageTitle;
+import com.vaadin.flow.router.Route;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
 import ru.anton.tester.backend.entity.Question;
 import ru.anton.tester.backend.repository.CorrectAnswerRepository;
 import ru.anton.tester.backend.repository.QuestionRepository;
 
 import java.util.List;
 
-@SpringComponent
-@UIScope
+@Route(value = "question")
+@Component
+@Scope("prototype")
+@PageTitle("Tester")
 public class QuestionForm extends VerticalLayout {
 
 
@@ -26,11 +35,13 @@ public class QuestionForm extends VerticalLayout {
     private final CorrectAnswerRepository correctAnswerRepository;
 
     List<Question> allQuestion;
+
+    Button btnAbort;
     Button btnAnswer;
     Button btnNextQuestion;
     H3 nameQuestion;
     RadioButtonGroup<String> radioTestOptions;
-    long ID = 1;
+    private  long ID = 1;
 
 
     @Autowired
@@ -42,37 +53,96 @@ public class QuestionForm extends VerticalLayout {
         btnAnswer = new Button("Ответить");
         btnNextQuestion = new Button("Следующий вопрос");
 
+
+
         allQuestion = questionRepository.findAll();
 
 
+        btnAbort = new Button("Прервать");
+
+        btnAnswer.addClickListener(e -> getCorrectAnswer());
+
+        btnAbort.addClickListener(e->{
+            UI.getCurrent().navigate(MainView.class);
+        });
 
         add(createQuestionLayout(ID), createButtonLayout());
 
 
+        btnNextQuestion.addClickListener(e->next());
+
     }
 
-    public void setID(long id) {
-        this.ID = id;
+    private void getCorrectAnswer() {
+        if(correctAnswerRepository.findById(ID).getCorrectAnswer().equals(radioTestOptions.getValue())){
+            Notification correctNotice = new Notification();
+            Span corrContent = new Span("Верный ответ");
+            Button btnCorrect = new Button("Закрыть", e -> correctNotice.close());
+            correctNotice.setDuration(3000);
+            correctNotice.setPosition(Notification.Position.MIDDLE);
+            correctNotice.addThemeVariants(NotificationVariant.LUMO_SUCCESS);
+            correctNotice.add(corrContent, btnCorrect);
+
+            corrContent.getStyle().set("margin-right", "0.5rem");
+            btnCorrect.getStyle().set("margin-right", "0.5rem");
+            correctNotice.open();
+        }else{
+            Notification errNotice = new Notification();
+            Span errContent = new Span("Неверный ответ");
+            Button btnErrCorrect = new Button("Закрыть", e -> errNotice.close());
+            errNotice.setDuration(3000);
+            errNotice.setPosition(Notification.Position.MIDDLE);
+            errNotice.addThemeVariants(NotificationVariant.LUMO_ERROR);
+            errNotice.add(errContent, btnErrCorrect);
+
+            errContent.getStyle().set("margin-right", "0.5rem");
+            btnErrCorrect.getStyle().set("margin-right", "0.5rem");
+            errNotice.open();
+        }
+    }
+
+    private void next() {
+        ID = ID + 1;
+        if (ID > 190)
+            ID = 1;
+        radioTestOptions.setValue(null);
+        nameQuestion.setText(questionRepository.findById(ID).getId() + ". " + questionRepository.findById(ID).getQuestion());
+        radioTestOptions.setItems(questionRepository.findById(ID).getTestOptions());
     }
 
 
     private VerticalLayout createQuestionLayout(long ID) {
-        //this.ID = ID;
         radioTestOptions = new RadioButtonGroup<>();
+        radioTestOptions.addThemeVariants(RadioGroupVariant.LUMO_VERTICAL);
         nameQuestion = new H3(questionRepository.findById(ID)
                 .getId() + ". " + questionRepository.findById(ID)
                 .getQuestion());
         radioTestOptions.setItems(questionRepository.findById(ID)
                 .getTestOptions());
-        return new VerticalLayout(nameQuestion, radioTestOptions);
 
+        radioTestOptions.addValueChangeListener(e->{
+            if(e.getValue() != null){
+                btnNextQuestion.setEnabled(true);
+                btnAnswer.setEnabled(true);
+            }else {
+                btnNextQuestion.setEnabled(false);
+                btnAnswer.setEnabled(false);
+            }
+        });
+
+        return new VerticalLayout(nameQuestion, radioTestOptions);
     }
 
 
     private HorizontalLayout createButtonLayout() {
-
         btnNextQuestion.addThemeVariants(ButtonVariant.LUMO_PRIMARY);
-        btnAnswer.addThemeVariants(ButtonVariant.LUMO_CONTRAST);
-        return new HorizontalLayout(btnNextQuestion, btnAnswer);
+        btnAnswer.addThemeVariants(ButtonVariant.LUMO_PRIMARY);
+        btnAbort.addThemeVariants(ButtonVariant.LUMO_PRIMARY);
+
+        if(radioTestOptions.getValue() == null){
+            btnNextQuestion.setEnabled(false);
+            btnAnswer.setEnabled(false);
+        }
+        return new HorizontalLayout(btnAnswer, btnNextQuestion, new HorizontalLayout(btnAbort));
     }
 }
